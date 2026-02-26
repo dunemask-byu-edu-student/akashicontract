194134e9f7a8284c5dbf1441d1427576d4f34d31
austinbeckstead <austinbeckstead314@gmail.com>
2026-02-25 19:42:16 -0700

    Added Contracts table to schema and wrote a service to query it

--- server/src/modules/core/contracts/contracts.service.ts
+++ server/src/modules/core/contracts/contracts.service.ts
@@ -1,6 +1,38 @@
+import { PostgresService } from "@akc/modules/common/postgres/postgres.service";
 import { Injectable } from "@nestjs/common";
+import { Contract } from "src/atlas/contracts/assets/contract.models";
 
 @Injectable()
 export class ContractsService {
-  constructor() {}
+  constructor(private readonly postgresService: PostgresService) {}
+
+  async getContract(id: string): Promise<Contract | null> {
+    return await this.postgresService.contracts.findUnique({ where: { id } });
+  }
+
+  async createContract(req: Contract): Promise<Contract> {
+    return await this.postgresService.contracts.create({
+      data: {
+        id: req.id,
+        userId: "1",
+        name: req.name,
+        attributes: req.attributes,
+      },
+    });
+  }
+
+  async updateContract(req: Contract): Promise<Contract> {
+    return await this.postgresService.contracts.update({
+      where: { id: req.id },
+      data: {
+        name: req.name,
+        attributes: req.attributes,
+      },
+    });
+  }
+
+  async deleteContract(id: string): Promise<void> {
+    await this.postgresService.contracts.delete({ where: { id } });
+  }
+
 }
