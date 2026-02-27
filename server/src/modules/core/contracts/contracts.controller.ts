16ca1cb06624a353d4087b3e87431948a5636620
austinbeckstead <austinbeckstead314@gmail.com>
2026-02-26 22:11:47 -0700

    Added paginated getContracts

--- server/src/modules/core/contracts/contracts.controller.ts
+++ server/src/modules/core/contracts/contracts.controller.ts
@@ -1,6 +1,6 @@
-import { Controller, Post, Get, Body, Query, UseGuards, Put, Delete } from "@nestjs/common";
+import { Controller, Post, Get, Body, Query, UseGuards, Put, Delete, Param } from "@nestjs/common";
 import { ContractsService } from "./contracts.service";
-import { Contract } from "@atlas/contracts";
+import { Contract, ContractRequest } from "@atlas/contracts";
 import { ResourcePolicyGuard } from "../../../util/nest-extras/resource-policy.guard";
 import { AtlasPolicyMap } from "@atlas/enums";
 
@@ -9,25 +9,36 @@ import { AtlasPolicyMap } from "@atlas/enums";
 export class ContractsController {
   constructor(private readonly contractsService: ContractsService) {}
 
-  @Get()
+  @Get(':id')
   async getContract(
-    @Query('id') id: string,
+    @Param('id') id: string,
   ): Promise<Contract | null> {
     return await this.contractsService.getContract(id);
   }
 
+  @Get()
+  async getContracts(
+    @Query('page') page?: string,
+    @Query('pageSize') pageSize?: string,
+  ): Promise<Contract[]> {
+    const currentPage = page ? parseInt(page, 10) : 1;
+    const currentPageSize = pageSize ? parseInt(pageSize, 10) : 5;
+    return await this.contractsService.getContracts("1", currentPage, currentPageSize);
+  }
+
   @Post()
   async createContract(
-    @Body() body: Contract,
+    @Body() body: ContractRequest,
   ): Promise<Contract> {
     return await this.contractsService.createContract(body);
   }
 
   @Put()
   async updateContract(
-    @Body() body: Contract,
+    @Body() body: ContractRequest,
+    @Query('id') id: string,
   ): Promise<Contract> {
-    return await this.contractsService.updateContract(body);
+    return await this.contractsService.updateContract(body, id);
   }
 
   @Delete()
--- server/src/modules/core/contracts/contracts.service.ts
+++ server/src/modules/core/contracts/contracts.service.ts
@@ -1,6 +1,6 @@
 import { PostgresService } from "@akc/modules/common/postgres/postgres.service";
 import { BadRequestException, Injectable, NotFoundException } from "@nestjs/common";
-import { Contract } from "src/atlas/contracts/assets/contract.models";
+import { Contract, ContractRequest } from "src/atlas/contracts/assets/contract.models";
 
 @Injectable()
 export class ContractsService {
@@ -14,10 +14,22 @@ export class ContractsService {
     return res;
   }
 
-  async createContract(contract: Contract): Promise<Contract> {
+  async getContracts(userId: string, page: number, pageSize: number): Promise<Contract[]> {
+    const offset = (page - 1) * pageSize;
+    return await this.postgresService.contracts.findMany({
+      where: { userId },
+      skip: offset,
+      take: pageSize,
+    });
+  }
+
+  async createContract(contractRequest: ContractRequest): Promise<Contract> {
     try {
       return await this.postgresService.contracts.create({
-        data: contract
+        data: {
+          ...contractRequest,
+          userId: "1",
+        }
       });
     } catch (error) {
       if (error instanceof Error) {
@@ -27,11 +39,14 @@ export class ContractsService {
     }
   }
 
-  async updateContract(contract: Contract): Promise<Contract> {
+  async updateContract(contractRequest: ContractRequest, id: string): Promise<Contract> {
     try {
       return await this.postgresService.contracts.update({
-        where: { id: contract.id },
-        data: contract,
+        where: { id },
+        data: {
+          ...contractRequest,
+          userId: "1",
+        },
       });
     } catch (error) {
       if (error instanceof Error) {
