de2eaae06f7bbe8d0cbc9d9fb1898d943c18b6a2
austinbeckstead <austinbeckstead314@gmail.com>
2026-02-27 00:58:23 -0700

    Added socketio service

--- server/src/modules/core/contracts/contracts.gateway.ts
+++ server/src/modules/core/contracts/contracts.gateway.ts
@@ -1,6 +1,18 @@
-import { WebSocketGateway } from "@nestjs/websockets/decorators/socket-gateway.decorator";
+import { WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
+import { Server } from "socket.io";
+import { Contract } from "src/atlas/contracts/assets/contract.models";
 
-@WebSocketGateway()
+@WebSocketGateway({ cors: { origin: "*" } })
 export class ContractsGateway {
-  constructor() {}
+  
+  @WebSocketServer()
+  server!: Server;
+
+  sendContractSet(contract: Contract) {
+    this.server.emit("contract.set", { id: contract.id, data: contract });
+  }
+
+  sendContractDelete(id: string) {
+    this.server.emit("contract.del", { id }) 
+  }
 }
