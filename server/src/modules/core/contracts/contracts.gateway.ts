0d7931d3879b3a19d30d3acb6b370fb844b7c7fc
austinbeckstead <austinbeckstead314@gmail.com>
2026-02-28 01:27:41 -0700

    Added logic to send all contracts to a client upon connecting, up to a constant limit

--- server/src/modules/core/contracts/contracts.gateway.ts
+++ server/src/modules/core/contracts/contracts.gateway.ts
@@ -1,12 +1,25 @@
-import { WebSocketGateway, WebSocketServer, SubscribeMessage } from "@nestjs/websockets";
-import { Server } from "socket.io";
+import { WebSocketGateway, WebSocketServer, SubscribeMessage, OnGatewayConnection } from "@nestjs/websockets";
+import { Server, Socket } from "socket.io";
+import { ContractsService } from "./contracts.service";
 import { Contract } from "src/atlas/contracts/assets/contract.models";
 
+const MAX_CONTRACTS_INIT = 1000;
+
 @WebSocketGateway({ cors: { origin: "*" } })
-export class ContractsGateway {
+export class ContractsGateway implements OnGatewayConnection {
   
   @WebSocketServer()
   server!: Server;
+
+  constructor(private readonly contractsService: ContractsService) {}
+
+  async handleConnection(client: Socket) {
+    const userId = "1" // Hardcoded until auth logic is implemented 
+    if (userId) {
+      const contracts = await this.contractsService.getContracts(userId, 1, MAX_CONTRACTS_INIT);
+      client.emit("contracts.init", { data: contracts });
+    }
+  }
 
   @SubscribeMessage("client.healthy")
   handleClientHealthy() {
