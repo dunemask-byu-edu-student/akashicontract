088ff3d85fc5371f3bfb10382644c64509b1c007
Dunemask <dunemask@gmail.com>
2026-02-27 19:21:14 -0700

    [FEATURE] Working Client

--- client/src/config/cli/cli-args.ts
+++ client/src/config/cli/cli-args.ts
@@ -16,11 +16,11 @@ program.parse(process.argv);
 const options = program.opts();
 
 export function loadOptions() {
-  if (options.config) config.set("configPath", options.config);
-  if (options.initConfig) config.set("initConfig", true);
-  if (options.pull) config.set("pull", true);
-  if (options.watch) config.set("watch", true);
-  if (options.production) config.set("production", true);
+  if (!!options.config) config.set("configPath", options.config);
+  if (!!options.initConfig) config.set("initConfig", true);
+  if (!!options.pull) config.set("pull", true);
+  if (!!options.watch) config.set("watch", true);
+  if (!!options.production) config.set("production", true);
 }
 
 export function displayHelp() {
